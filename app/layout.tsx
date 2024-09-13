99782f1a9d0d13f8b74b7ccbffc6ab3cd1125669
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-13 15:55:15 +0200

    Add suspense to layout to fix build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Suspense } from 'react';
 import ContactList from '@/components/ContactList';
 import Search from '@/components/Search';
 import Button from '@/components/ui/Button';
@@ -24,13 +25,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
         <div className="flex w-full flex-col sm:flex-row">
           <div className="flex h-1/3 w-full flex-col border-r border-gray bg-gray-light sm:h-auto sm:w-[18rem] md:w-[22rem]">
-            <div className="flex items-center gap-2 border-b border-gray px-8 py-4">
-              <Search />
-              <Button type="submit" theme="secondary">
-                New
-              </Button>
-            </div>
-            <ContactList />
+            <Suspense>
+              <div className="flex items-center gap-2 border-b border-gray px-8 py-4">
+                <Search />
+                <Button type="submit" theme="secondary">
+                  New
+                </Button>
+              </div>
+              <ContactList />
+            </Suspense>
             <div className="m-0 hidden flex-row items-center gap-2 border-t border-t-gray px-8 py-4 font-medium sm:flex">
               <Link className="flex items-center gap-2 text-black no-underline" href="/">
                 <Image priority width={30} height={30} src={Logo} alt="Next.js logo" />
