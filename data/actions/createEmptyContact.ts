8bd880f0ae081be393a9083d469bba0067fcf2ec
Denis Magazeishchikov <denmagazeishchikov@gmail.com>
2025-05-26 14:16:58 +0300

    Add createEmptyContact action and form for creating new contacts

---
+++ data/actions/createEmptyContact.ts
@@ -0,0 +1,15 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { prisma } from '@/db';
+
+export const createEmptyContact = async () => {
+  const contact = await prisma.contact.create({
+    data: {},
+  });
+
+  revalidatePath('/');
+
+  redirect(`/contacts/${contact.id}/edit`);
+};
